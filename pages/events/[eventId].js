1a44b1bccf1dff0aee9f943341659fb9a56b78a5
Sarah Hanafy <sarahhanafy20@gmail.com>
2023-11-06 15:54:34 +0200

    implement event details page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,31 @@
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+
 function EventDetailPage() {
-    return <div>
-        <h1>Event Detail Page</h1>
-    </div>
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 
 export default EventDetailPage;
