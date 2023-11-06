eeaa09712c52123ae2f9301d9b203a6f6795fb1a
Sarah Hanafy <sarahhanafy20@gmail.com>
2023-11-06 15:44:41 +0200

    implement event item ui

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
+import EventList from "@/components/events/event-list";
+import { getFeaturedEvents } from "@/dummy-data";
 
 export default function HomePage() {
-  return (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-  )
+  const featuredEvents = getFeaturedEvents();
+
+  return <EventList items={featuredEvents} />;
 }
